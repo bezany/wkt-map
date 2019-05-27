103ad3a5515b7400b8d31eb9e318755323ce1399
bezany <bezanyu@gmail.com>
2019-05-27 18:07:54 +0300

    fix config for correct fonts path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -72,7 +72,9 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          // fix problem with fonts https://github.com/vuejs-templates/webpack/issues/1284
+          publicPath: process.env.NODE_ENV === 'production' ? '../../' : '/'
         }
       }
     ]
